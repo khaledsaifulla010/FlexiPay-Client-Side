dff41037912f814a9a93bc2cbdadfe485d5d837b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-24 17:57:30 +0600

    added register page

---
+++ src/authentication/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div className="bg-white min-h-screen">
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         inquiries with a secure, user-friendly experience. Enjoy seamless
         digital transactions anytime, anywhere!
       </p>
-      <Link className="w-36 mt-8 p-2 border rounded-md font-bold text-lg ml-[700px] bg-indigo-800 border-indigo-700 cursor-pointer flex items-center justify-center gap-1.5">
+      <Link to='/register' className="w-36 mt-8 p-2 border rounded-md font-bold text-lg ml-[700px] bg-indigo-800 border-indigo-700 cursor-pointer flex items-center justify-center gap-1.5">
         Get Started <FaHandPointRight />
       </Link>
     </div>
