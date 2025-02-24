9844bb42ed12d201f4c79ce4d66ba0c0c8fc797e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-24 17:47:27 +0600

    added welcome texts in home page

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 
-
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div className="bg-green-900 min-h-screen ">
+      <Outlet />
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
   return (
-    <div>
-      <h1>This is My Home</h1>
+    <div className="text-white font-1">
+      <h1 className="text-6xl font-bold  text-center py-52  ">
+        FlexiPay – Fast, Secure & Effortless Transactions
+      </h1>
+      <p className="text-2xl font-semibold -mt-44 text-center px-40">
+        FlexiPay simplifies sending money, cash-in, cash-out, and balance
+        inquiries with a secure, user-friendly experience. Enjoy seamless
+        digital transactions anytime, anywhere!
+      </p>
+      <button className="mt-8 p-2 border rounded-md font-bold text-lg ml-[700px] bg-indigo-800 border-indigo-500 cursor-pointer">
+        Get Started
+      </button>
     </div>
   );
 };
