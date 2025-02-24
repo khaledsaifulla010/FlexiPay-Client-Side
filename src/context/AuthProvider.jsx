6f8845e05f67948ec6448086099e6eccb6ed7f7b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-24 22:40:47 +0600

    added observer for tracking current user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,21 +1,40 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
 import AuthContext from "./AuthContext";
 import auth from "../firebase/firebase.config";
+import { useEffect, useState } from "react";
 
 const AuthProvider = ({ children }) => {
-  // Register With Email & Pin //
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  // Register With Email & Pin //
   const createUser = (email, pin) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, pin);
   };
 
   // Update User Profile //
-
   const updateUserProfile = (name) => {
     return updateProfile(auth.currentUser, {
       displayName: name,
     });
   };
+
+  // Current User Observer //
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log("Logged in User", currentUser);
+    });
+    return () => {
+      unSubscribe;
+    };
+  }, []);
 
   const authInfo = {
     createUser,
