0c67f1c73f3351d73d3e2ae2df1cf50426c90bf5
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-24 17:31:16 +0600

    added layouts for projects structure

---
+++ src/pages/UserAccount/userHome/userHome.jsx
@@ -0,0 +1,9 @@
+const userHome = () => {
+  return (
+    <div>
+      <h1>User Home</h1>
+    </div>
+  );
+};
+
+export default userHome;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="font-1 text-3xl">Hello world!</div>,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
