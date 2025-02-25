45e82fa8a3a2277a734817b0fba0aae9d033ce01
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-25 10:43:49 +0600

    added all navlink in user layout

--- src/layout/UserLayout/UserLayout.jsx
+++ src/layout/UserLayout/UserLayout.jsx
@@ -1,10 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { FaHandHoldingDollar, FaMoneyBillTransfer } from "react-icons/fa6";
+import { RiHome9Fill } from "react-icons/ri";
+import { TbFolderDollar } from "react-icons/tb";
+import { NavLink, Outlet } from "react-router-dom";
 
 const UserLayout = () => {
   return (
-    <div className="flex w-full ">
+    <div className="flex w-full font-2 text-white">
       {/* Side Content */}
-      <div className="w-[350px] border border-red-500 min-h-screen  bg-[#171717]"></div>
+      <div className="w-[350px] border border-red-500 min-h-screen space-y-8 bg-[#171717] py-12">
+        <NavLink
+          to="userHome"
+          className="flex items-center font-bold text-2xl gap-1.5 ml-20"
+        >
+          <RiHome9Fill />
+          Home
+        </NavLink>
+        <NavLink
+          to="userHome"
+          className="flex items-center font-bold text-2xl gap-1.5 ml-20"
+        >
+          <TbFolderDollar />
+          Cash In
+        </NavLink>
+        <NavLink
+          to="userHome"
+          className="flex items-center font-bold text-2xl gap-1.5 ml-20"
+        >
+          <FaMoneyBillTransfer />
+          Send Money
+        </NavLink>
+        <NavLink
+          to="userHome"
+          className="flex items-center font-bold text-2xl gap-1.5 ml-20"
+        >
+          <FaHandHoldingDollar />
+          Cash Out
+        </NavLink>
+      </div>
 
       {/* Main Content */}
       <div className="w-full border border-green-700  bg-[#212121]">
--- src/pages/UserAccount/userHome/userHome.jsx
+++ src/pages/UserAccount/userHome/userHome.jsx
@@ -2,7 +2,6 @@ const UserHome = () => {
   return (
     <div>
       <h1>User Home</h1>
-      {/*  */}
     </div>
   );
 };
